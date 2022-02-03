0ba66bba9eada8067d15ab409a4853e29207966b
dingzuosheng <zuoshengding@gmail.com>
2022-02-03 13:07:55 +0100

    add ArraList in C#

---
+++ vl13/ArrayList/ArrayList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace ArrayList
+{
+    public class ArrayList
+    {
+        private double[] feld;
+        public int Capacity { get; private set; }
+        public int Count { get; private set; }
+
+        public ArrayList()
+        {
+            Capacity = 1;
+            feld = new double[Capacity];
+            Count = 0;
+        }
+        private void Resize(int size)
+        {
+            double[] neu = new double[size];
+            for (int i = 0; i < Count; i++)
+            {
+                neu[i] = feld[i];
+            }
+            feld = neu;
+            Capacity = size;
+        }
+        public void Insert(int pos, double key)
+        {
+            if (pos < 0 || pos > Count)
+            {
+                throw new Exception("Index out of range !");
+            }
+            if (pos == Capacity)
+            {
+                Resize(2 * Capacity);
+            }
+            if (pos == Count)
+            {
+                Count++;
+            }
+            feld[pos] = key;//warum hier count nicht um 1 erhöhen? weil das ist nicht eine LinkedList, 
+                            //wenn in der Mittel der List eine Zahl hinzugefügt wird, die originale Zahl wird nur überschrieben. 
+        }
+
+        public void Add(double key)
+        {
+            Insert(Count, key);
+        }
+
+        public double Get(int pos)
+        {
+            if (pos < 0 || pos > Count)
+            {
+                throw new Exception("Index-Error");
+            }
+            return feld[pos];
+        }
+    }
+}
---
+++ vl13/ArrayList/Programm.cs
@@ -0,0 +1,21 @@
+using System;
+namespace ArrayList
+{
+    public class MainProgramm
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+
+            for (int i = 0; i < 100; i++)
+            {
+                liste.Add(1.0 * i);
+                Console.WriteLine("Count = {0}, Capacity = {1}", liste.Count, liste.Capacity);
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(liste.Get(i));
+            }
+        }
+    }
+}
