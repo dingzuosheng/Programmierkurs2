82e93cf88b6cdec871f5906cb6aced9c75bfa996
dingzuosheng <zuoshengding@gmail.com>
2022-02-03 17:50:27 +0100

    add Delegate

---
+++ vl13/Delegate/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Delegate
+{
+    public class MainProgramm
+    {
+        delegate int Calc(int a, int b);
+
+        public static int Summe(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Produkt(int x, int y)
+        {
+            return x * y;
+        }
+        public static void Main(string[] args)
+        {
+            Calc c = new Calc(Summe);
+
+            Console.WriteLine("100 + 10 = {0}", c(100, 10));
+
+            c = Produkt;
+
+            Console.WriteLine("100 * 15 = {0}", c(100, 15));
+
+            c = delegate (int a, int b)
+            {
+                return a - b;
+            };
+
+            Console.WriteLine("100 - 10 = {0}", c(100, 10));
+
+            c = (int a, int b) => a / b; // c = (a, b) => a / b;
+
+            Console.WriteLine("100 / 10 = {0}", c(100, 10));
+        }
+    }
+}
