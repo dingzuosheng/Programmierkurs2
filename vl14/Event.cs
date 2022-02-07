9823b97aad64e1c1452a2dc0a0b47e4ec3970b3e
dingzuosheng <zuoshengding@gmail.com>
2022-02-07 20:48:33 +0100

    Observer Muster mit Delegate und Event

---
+++ vl14/Event.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+namespace Event
+{
+    public delegate void RandomNumverHandler(int i);
+
+
+    public class Min
+    {
+        private int min;
+
+        public Min(RandomGenerator rg)
+        {
+            min = int.MaxValue;
+            rg.RandomNumber += this.Action;
+        }
+        public void Action(int i)
+        {
+            if (i < min)
+            {
+                Console.WriteLine("Min = {0}", i);
+                min = i;
+            }
+        }
+    }
+
+    public class Max
+    {
+        private int max;
+
+        public Max(RandomGenerator rg)
+        {
+            max = int.MinValue;
+            rg.RandomNumber += this.Action;
+        }
+        public void Action(int i)
+        {
+            if (i > max)
+            {
+                Console.WriteLine("Max = {0}", i);
+                max = i;
+            }
+        }
+    }
+    public class RandomGenerator
+    {
+        public event RandomNumverHandler RandomNumber;
+        public void Start()
+        {
+            Random rg = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int z = rg.Next(10);
+                RandomNumber(z);
+            }
+        }
+    }
+}
--- vl14/Program.cs
+++ vl14/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Delegate;
+using Event;
 
 namespace Main
 {
