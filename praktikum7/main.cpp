a36a3f44002e78e0aeafd476a7afcabd64ae2f0d
dingzuosheng <zuoshengding@gmail.com>
2022-02-14 17:34:57 +0100

    fix kommentar in main

--- praktikum7/main.cpp
+++ praktikum7/main.cpp
@@ -6,7 +6,7 @@ int main(){
     Medium* medien[2];
 
     Audio a = Audio("It Means Nothing", 2007,"Stereophonics",229); // auf static stack gespeichert, wenn Main fertig ausgefuerht wurde, dann ist die Variable  auch weg.
-    //Audio a = Audio("It Means Nothing", 2007,"Stereophonics",229);  in heap fest gespeichert, muss mit delete aufräumen
+    //Audio a = new Audio("It Means Nothing", 2007,"Stereophonics",229);  in heap fest gespeichert, muss mit delete aufräumen
     Bild b = Bild("Gebaeude FB Informatik", 2020,"Dortmund");
  
     medien[0] = &a; 
